8e561ad45fc4762f1dcc605ca34b7dab63f02fa7
Nikita Spekhin <nikitos.0@list.ru>
2023-12-10 12:32:11 +0500

    first commit

---
+++ OTUS/OTUSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTUSApp.swift
+//  OTUS
+//
+//  Created by Nikita Spekhin on 10.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct OTUSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
